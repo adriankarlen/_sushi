20fa0b4d856c245a2104cc977dd165456421f0de
Adrian Karlén <113891532+adriankarlen-spp@users.noreply.github.com>
2022-11-17 15:13:41 +0100

    Changed city to stockholm for weather

--- js/main.js
+++ js/main.js
@@ -1,27 +1,46 @@
 function dateTime() {
-	const date = new Date();
-	let today = date.toDateString();
-	let time = date.toLocaleTimeString();
-	document.getElementById('date-time').innerHTML = '<p id="date">' + today + '</p><p id="time">' + time + '</p>';
-	setTimeout(dateTime, 1000);
+    const date = new Date();
+    let today = date.toDateString();
+    let time = date.toLocaleTimeString();
+    document.getElementById('date-time').innerHTML =
+        '<p id="date">' + today + '</p><p id="time">' + time + '</p>';
+    setTimeout(dateTime, 1000);
 }
 
 function weatherBalloon(cityID) {
-	var apiKey = ''; //OpenWeather API key
-	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + apiKey)
-		.then(function(resp) {
-			return resp.json()
-		})
-		.then(function(data) {
-			let weatherIcon = data.weather[0].icon;
-			let tempK = parseFloat(data.main.temp);
-			let tempC = Math.round(tempK - 273.15);
-			let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
-			document.getElementById('weather').innerHTML = '<p id="location">' + data.name + '</p><p id="details" ' + 'title="' + tempF + '&deg;F">' + '<img src="https://openweathermap.org/img/wn/' + weatherIcon + '.png">' + data.weather[0].description + '<span class="separator">|</span>' + tempC + '&deg;C</p>';
-		});
+    var apiKey = ''; //OpenWeather API key
+    fetch(
+        'https://api.openweathermap.org/data/2.5/weather?id=' +
+            cityID +
+            '&appid=' +
+            apiKey
+    )
+        .then(function (resp) {
+            return resp.json();
+        })
+        .then(function (data) {
+            let weatherIcon = data.weather[0].icon;
+            let tempK = parseFloat(data.main.temp);
+            let tempC = Math.round(tempK - 273.15);
+            let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
+            document.getElementById('weather').innerHTML =
+                '<p id="location">' +
+                data.name +
+                '</p><p id="details" ' +
+                'title="' +
+                tempF +
+                '&deg;F">' +
+                '<img src="https://openweathermap.org/img/wn/' +
+                weatherIcon +
+                '.png">' +
+                data.weather[0].description +
+                '<span class="separator">|</span>' +
+                tempC +
+                '&deg;C</p>';
+        });
 }
 
-function traichu() {
-	dateTime();
-	weatherBalloon(1850147); //OpenWeather city ID
+function start() {
+    dateTime();
+    weatherBalloon(2673730); //OpenWeather city ID
 }
