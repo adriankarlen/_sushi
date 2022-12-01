6c07be5025bb69253e86e119b1a5033441ff066a
Adrian Karlén <113891532+adriankarlen-spp@users.noreply.github.com>
2022-12-01 14:49:15 +0100

    Added datetime config

--- src/components/DateTime/index.jsx
+++ src/components/DateTime/index.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import config from "./config.json";
+
 
 export default function DateTime() {
     const [date, setDate] = useState(new Date());
@@ -8,7 +10,7 @@ export default function DateTime() {
     };
 
     useEffect(() => {
-        const timerID = setInterval(() => tick(), 1000);
+        const timerID = setInterval(() => tick(), 30000); // Update clock every 30 seconds
         return function cleanup() {
             clearInterval(timerID);
         };
@@ -17,8 +19,8 @@ export default function DateTime() {
     return (
         <section id='date-time'>
             <h2 className='sr-only'>Date &amp; Time</h2>
-            <p id='date'>{date.toLocaleDateString()}</p>
-            <p id='time'>{date.toLocaleTimeString()}</p>
+            <p id='date'>{date.toLocaleDateString(config.lang, config.dateOptions)}</p>
+            <p id='time'>{date.toLocaleTimeString(config.timeFormat, config.timeOptions)}</p>
         </section>
     );
 }
