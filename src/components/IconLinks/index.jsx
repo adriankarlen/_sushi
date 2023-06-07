d60cded33d31efe797a30744e0c00a434be8506f
adriankarlen <18417539+adriankarlen-old@users.noreply.github.com>
2023-06-07 14:45:50 +0200

    Added geolocation (#10)
    Co-authored-by: = <=>

--- src/components/IconLinks/index.jsx
+++ src/components/IconLinks/index.jsx
@@ -11,9 +11,9 @@ export default function IconLinks() {
                             viewBox='0 0 24 24'
                             width={24}
                             height={24}
-                            stroke-width={2}
-                            stroke-linecap='round'
-                            stroke-linejoin='round'
+                            strokeWidth={2}
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
                             fill='none'
                             xmlns='http://www.w3.org/2000/svg'
                             preserveAspectRatio='xMidYMid meet'>
