f64777c0a89be70e1d1c4619602a14571e18417f
= <=>
2023-06-20 10:43:21 +0200

    moved link colors to js-array

--- src/common/components/IconLinks.jsx
+++ src/common/components/IconLinks.jsx
@@ -1,15 +1,25 @@
 import links from '../data/links';
 
 export const IconLinks = () => {
+    const colorMap = [
+        'hover:stroke-ctp-red focus:stroke-ctp-red/25',
+        'hover:stroke-ctp-peach focus:stroke-ctp-peach/25',
+        'hover:stroke-ctp-yellow focus:stroke-ctp-yellow/25',
+        'hover:stroke-ctp-green focus:stroke-ctp-green/25',
+        'hover:stroke-ctp-blue focus:stroke-ctp-blue/25',
+        'hover:stroke-ctp-mauve focus:stroke-ctp-mauve/25',
+        'hover:stroke-ctp-text focus:stroke-ctp-text/25'
+    ];
+
     return (
         <ul className='flex flex-row flex-wrap justify-center list-none gap-x-4 gap-y-4 p-0 md:column-gap-0 md:justify-around md:p-0'>
-            {links.map((link) => (
+            {links.map((link, index) => (
                 <li key={link.title}>
                     <a
                         href={link.url}
                         target='_self'
                         rel='noopener noreferrer'
-                        className={`block stroke-ctp-surface2 transition-fill duration-125 ease-in-out hover:stroke-${link.hoverColor} focus:stroke-${link.hoverColor} focus:outline-none`}>
+                        className={`block stroke-ctp-surface2 transition-fill duration-125 ease-in-out ${colorMap[index]} focus:outline-none`}>
                         <svg
                             className='h-6'
                             role='img'
